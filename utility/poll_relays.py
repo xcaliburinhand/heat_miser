5f11f5b5906dcb4e621df023b5268c6aedf51168
xcaliburinhand <github@muteheadlight.net>
2017-01-28 23:55:16 -0500

    initial

---
+++ utility/poll_relays.py
@@ -0,0 +1,31 @@
+
+import json
+import logging
+
+hd=False
+
+try:
+  with open("config.json") as config_file:
+    config = json.load(config_file)
+except Exception as e:
+  logging.error("Config file error {0}".format(e))
+  exit(99)  
+
+if hd==True:
+  header = 'burner  '
+  result = '1'.ljust(8)
+else:
+  result='1,'
+
+for zone in config['zones']:
+  if hd==True:
+    header += zone+'  '
+    result += '0'.ljust(2+len(zone))
+  else:
+    result += '0,'
+  
+try:
+  print(header)
+except:
+  pass
+print(result)
