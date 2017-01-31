dae59bbb3787507369ec65175764c0fe1599e1da
xcaliburinhand <github@muteheadlight.net>
2017-01-30 23:29:11 -0500

    write equipment current status

--- utility/build_dataset.py
+++ utility/build_dataset.py
@@ -4,6 +4,7 @@ import csv
 import datetime
 import json
 import logging
+from operator import itemgetter
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,10 +26,13 @@ equip.remove('time')
 
 status_store={}
 rows =[]
+last_five_rows=[None]*5
+i=0
 
 with open('fake_data') as csvfile:
   reader = csv.DictReader(csvfile,fieldnames=fields)
   for row in reader:
+    last_five_rows[i % 5]=row
     logging.debug(row)
     for device in equip:
       logging.debug("row value for %s is %s",device,row[device])
@@ -53,6 +57,7 @@ with open('fake_data') as csvfile:
         if device not in status_store or status_store[device]==None:
           logging.debug('setting %s to on in status_store',device)
           status_store[device]=row['time']
+    i+=1
 
 # drain status_store
 for device in equip:
@@ -69,7 +74,19 @@ for device in equip:
       datetime.datetime.strptime(row['time'],"%Y-%m-%d %H:%M").strftime(', %d, %H, %M)')
       ,"f":None}]})
 
+# get current device status
+last_five_rows = sorted(last_five_rows, key=itemgetter('time'), reverse=True) 
+logging.debug("last five rows: %s",last_five_rows)
+current_status={}
+for device in equip:
+  for i in range(0,5):
+    logging.debug("state of %s at %s is %s",device,last_five_rows[i]['time'],last_five_rows[i][device])
+    if last_five_rows[i][device] == '0' or last_five_rows[i][device] == '1':
+      current_status[device]=last_five_rows[i][device]
+      break
+
+with open('../www/data/current_state.json','w') as out:
+  json.dump(current_status,out)
 dataTable={"cols":cols,"rows":rows}
 with open('../www/data/20170130.json','w') as out:
   json.dump(dataTable,out)
-logging.debug(json.dumps(dataTable))
